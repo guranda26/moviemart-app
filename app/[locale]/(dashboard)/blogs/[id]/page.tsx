e587e38d8be672db754bd396781be95944fa30e0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2025-01-28 04:52:16 +0400

    translate texts on teh blog details page

--- app/[locale]/(dashboard)/blogs/[id]/page.tsx
+++ app/[locale]/(dashboard)/blogs/[id]/page.tsx
@@ -1,10 +1,16 @@
 import NotFoundPage from "@/app/[locale]/not-found";
 import { Params } from "next/dist/server/request/params";
-import { Tag, Eye, Folder, ArrowLeft } from "lucide-react"; // Icons
+import { Tag, Eye, Folder, ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import initTranslations from "@/utils/i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
 
 const BlogPage = async ({ params }: { params: Params }) => {
+  const i18nNameSpaces = ["common", "products"];
+
   const { id, locale } = await params;
+
+  const { t, resources } = await initTranslations(locale, i18nNameSpaces);
 
   if (!id) {
     return <NotFoundPage />;
@@ -62,94 +68,100 @@ const BlogPage = async ({ params }: { params: Params }) => {
   const CATEGORY = isKa ? category_ka : category;
 
   return (
-    <section className="relative p-4">
-      <Link
-        href={"/blogs"}
-        className="absolute top-16 lg:top-4 left-4 my-2 xl:my-0 flex items-center gap-2 px-2 py-1 xs:px-4 xs:py-2 text-sm font-medium text-white bg-redButton rounded shadow hover:bg-hoverRedBtn transition-transform transform hover:scale-105 duration-300 z-50"
-      >
-        <ArrowLeft className="w-4 h-4 inline-block" />
-        <span className="hidden xl:inline-block">Go Back</span>
-      </Link>
+    <TranslationsProvider
+      resources={resources}
+      locale={locale as string}
+      namespaces={i18nNameSpaces}
+    >
+      <section className="relative p-4">
+        <Link
+          href={"/blogs"}
+          className="absolute top-16 lg:top-4 left-4 my-2 xl:my-0 flex items-center gap-2 px-2 py-1 xs:px-4 xs:py-2 text-sm font-medium text-white bg-redButton rounded shadow hover:bg-hoverRedBtn transition-transform transform hover:scale-105 duration-300 z-50"
+        >
+          <ArrowLeft className="w-4 h-4 inline-block" />
+          <span className="hidden xl:inline-block">{t("common:go_back")}</span>
+        </Link>
 
-      <article className="max-w-4xl mx-auto bg-articleBg shadow-md overflow-hidden animate-slide-in z-90">
-        <div className="flex flex-wrap gap-2 p-4 justify-center xs:justify-normal animate-slide-in">
-          {localizedTags.map(
-            (tag, index) =>
-              tag && (
-                <span
-                  key={index}
-                  className="flex items-center gap-2 bg-gray-100 text-gray-700 rounded-full px-3 py-1 text-xs font-medium transition-transform transform hover:scale-105 duration-300"
-                >
-                  <Tag className="w-4 h-4 text-gray-500" />
-                  {tag}
+        <article className="max-w-4xl mx-auto bg-articleBg shadow-md overflow-hidden animate-slide-in z-90">
+          <div className="flex flex-wrap gap-2 p-4 justify-center xs:justify-normal animate-slide-in">
+            {localizedTags.map(
+              (tag, index) =>
+                tag && (
+                  <span
+                    key={index}
+                    className="flex items-center gap-2 bg-gray-100 text-gray-700 rounded-full px-3 py-1 text-xs font-medium transition-transform transform hover:scale-105 duration-300"
+                  >
+                    <Tag className="w-4 h-4 text-gray-500" />
+                    {tag}
+                  </span>
+                )
+            )}
+          </div>
+          <img
+            src={imageSrc}
+            alt={TITLE}
+            className="w-full h-85 max-h-[450px] object-cover transform hover:scale-105 transition-transform duration-500"
+          />
+          <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4 text-gray-900 animate-fade-in-delayed">
+              {TITLE}
+            </h1>
+            <p className="text-sm text-gray-500 mb-2">{DATE}</p>
+            {DESCRIPTION.split(". ").map((paragraph: string, index: number) => (
+              <p
+                key={index}
+                className="text-base text-gray-800 mb-4 leading-relaxed"
+              >
+                {paragraph.trim() +
+                  (index !== DESCRIPTION.split(". ").length - 1 ? "." : "")}
+              </p>
+            ))}
+            <div className="w-100% overflow-x-hidden mb-2">
+              {additionalImageSrc && (
+                <img
+                  src={additionalImageSrc}
+                  alt={TITLE}
+                  className="w-full h-60 xs:h-[17rem] object-cover transform hover:scale-105 transition-transform duration-500 bg-top"
+                />
+              )}
+            </div>
+            {EXTRA_DESCRIPTION &&
+              EXTRA_DESCRIPTION.split(". ").map(
+                (paragraph: string, index: number) => (
+                  <p
+                    key={index}
+                    className="text-base text-gray-800 mb-4 leading-relaxed"
+                  >
+                    {paragraph.trim() +
+                      (index !== EXTRA_DESCRIPTION.split(". ").length - 1
+                        ? "."
+                        : "")}
+                  </p>
+                )
+              )}
+
+            <div className="flex items-center gap-6 text-gray-700 text-sm">
+              <div className="flex items-center gap-2 animate-fade-in-delayed">
+                <Folder className="w-4 h-4 text-gray-600" />
+                <span>{CATEGORY}</span>
+              </div>
+              <div className="flex items-center gap-2 animate-fade-in-delayed">
+                <Eye className="w-4 h-4 text-gray-600" />
+                <span className="font-semibold hover:scale-105">
+                  {views || "N/A"} views
                 </span>
-              )
-          )}
-        </div>
-        <img
-          src={imageSrc}
-          alt={TITLE}
-          className="w-full h-85 max-h-[450px] object-cover transform hover:scale-105 transition-transform duration-500"
-        />
-        <div className="p-6">
-          <h1 className="text-3xl font-bold mb-4 text-gray-900 animate-fade-in-delayed">
-            {TITLE}
-          </h1>
-          <p className="text-sm text-gray-500 mb-2">{DATE}</p>
-          {DESCRIPTION.split(". ").map((paragraph: string, index: number) => (
-            <p
-              key={index}
-              className="text-base text-gray-800 mb-4 leading-relaxed"
-            >
-              {paragraph.trim() +
-                (index !== DESCRIPTION.split(". ").length - 1 ? "." : "")}
-            </p>
-          ))}
-          <div className="w-100% overflow-x-hidden mb-2">
-            {additionalImageSrc && (
-              <img
-                src={additionalImageSrc}
-                alt={TITLE}
-                className="w-full h-60 xs:h-[17rem] object-cover transform hover:scale-105 transition-transform duration-500 bg-top"
-              />
+              </div>
+            </div>
+
+            {author && (
+              <p className="text-sm text-gray-600 mt-4 animate-fade-in-delayed">
+                <strong>{t("products:author")}:</strong> {author}
+              </p>
             )}
           </div>
-          {EXTRA_DESCRIPTION &&
-            EXTRA_DESCRIPTION.split(". ").map(
-              (paragraph: string, index: number) => (
-                <p
-                  key={index}
-                  className="text-base text-gray-800 mb-4 leading-relaxed"
-                >
-                  {paragraph.trim() +
-                    (index !== EXTRA_DESCRIPTION.split(". ").length - 1
-                      ? "."
-                      : "")}
-                </p>
-              )
-            )}
-
-          <div className="flex items-center gap-6 text-gray-700 text-sm">
-            <div className="flex items-center gap-2 animate-fade-in-delayed">
-              <Folder className="w-4 h-4 text-gray-600" />
-              <span>{CATEGORY}</span>
-            </div>
-            <div className="flex items-center gap-2 animate-fade-in-delayed">
-              <Eye className="w-4 h-4 text-gray-600" />
-              <span className="font-semibold hover:scale-105">
-                {views || "N/A"} views
-              </span>
-            </div>
-          </div>
-
-          {author && (
-            <p className="text-sm text-gray-600 mt-4 animate-fade-in-delayed">
-              <strong>Author:</strong> {author}
-            </p>
-          )}
-        </div>
-      </article>
-    </section>
+        </article>
+      </section>
+    </TranslationsProvider>
   );
 };
 
