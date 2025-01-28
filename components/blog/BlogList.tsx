aa23187c51e26bb797c82f49c68e54fa8ad2b66f
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2025-01-28 05:41:32 +0400

    add search for the blogs page

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -1,26 +1,70 @@
+"use client";
+
 import { BlogData } from "@/Interfaces/Blogs";
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import AutoPlayVideo from "../VideoAutoPlay";
 import Loading from "../Loading";
 import useLocaleFromPath from "../UsePath";
 import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+import Input from "../Input";
+import { useTranslation } from "react-i18next";
 
 const bannerUrl = "/assets/banner-video.mp4";
 
 const BlogList = ({ posts }: { posts: BlogData[] }) => {
   const locale = useLocaleFromPath();
 
-  console.log("fetch posts", locale);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const { t } = useTranslation();
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredPosts = posts.filter((post) => {
+    const localizedTitle = locale === "ka" ? post.title_ka : post.title;
+    const localizedTags =
+      locale === "ka"
+        ? [post.tags_ka__001, post.tags_ka__002, post.tags_ka__003]
+        : [post.tags__001, post.tags__002, post.tags__003];
+    const matchTitle = localizedTitle
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    const matchTags = localizedTags.some(
+      (tag) => tag && tag.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return matchTitle || matchTags;
+  });
 
   return (
     <section className="min-h-screen max-w-screen p-4">
-      <section className="text-textCol mx-auto">
+      <section className="text-textCol mx-auto relative">
         <Suspense fallback={<Loading />}>
           <AutoPlayVideo src={bannerUrl} width="auto" />
         </Suspense>
+        <div className="mb-[10px] my-4 md:absolute bottom-0 md:top-[145px] left-[30px] md:w-[350px]">
+          <div className="relative">
+            <Input
+              id="search"
+              name="search"
+              type="text"
+              placeholder={t("products:search_text")}
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="w-full p-2 pl-10 border rounded shadow-sm bg-[#363636] md:bg-inputCol text-white"
+            />
+            <FaSearch
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+              size={20}
+            />
+          </div>
+        </div>
       </section>
+
       <ul className="max-w-[1400px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-auto pt-4 sm:pt-2">
-        {posts.map(
+        {filteredPosts.map(
           ({
             id,
             title,
